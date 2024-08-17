330e1125c9ce906661f212c3eab7cd7909d05ee4
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-08-18 00:49:11 +0200

    resolve bag summary overlap

--- melsoft-website/src/components/bagSummary.js
+++ melsoft-website/src/components/bagSummary.js
@@ -1,15 +1,16 @@
 import React from "react";
 import dellxps from "../assets/Name=Dell-XPS-13-White.png";
-import bag from "../assets/Name=bag-handle.png"
+import bag from "../assets/Name=bag-handle.png";
 import Iphone12 from '../assets/Name=Iphone-12-Pro-02.png';
 import Iphone4 from '../assets/Name=Iphone-12-04.png';
 import Iphone1 from '../assets/Name=Iphone-12-01.png';
 
-const bagSummary = () => {
+const BagSummary = () => {
   return (
     <div className="p-3 align-items-center mt-5" style={{
-      width: '345px',
-      height: '80%',
+      width: '100%',
+      maxWidth: '345px',
+      height: 'auto',
       padding: '7px 8px 8px 8px',
       borderLeft: '2px solid gray',
       display: 'flex',
@@ -19,17 +20,17 @@ const bagSummary = () => {
       <h5 className="mb-4" style={{ marginBottom: '112px', fontSize: '30px' }}>Bag</h5>
       <div className="container">
         <div className="row">
-          <div className="col-4 mb-3">
-            <img src={dellxps} className="img-thumbnail" alt="" style={{ width: '80px', height: '80px', borderRadius: '12px' }} />
+        <div className="col-6 col-sm-4 mb-3">
+            <img src={dellxps} className="img-thumbnail" alt="" style={{ width: '100%', height: '100%', borderRadius: '12px' }} />
           </div>
-          <div className="col-4 mb-3">
-            <img src={Iphone12} className="img-thumbnail" alt="" style={{ width: '80px', height: '80px', borderRadius: '12px' }} />
+          <div className="col-6 col-sm-4 mb-3">
+            <img src={Iphone12} className="img-thumbnail" alt="" style={{ width: '100%', height: 'auto', borderRadius: '12px' }} />
           </div>
-          <div className="col-4 mb-3">
-            <img src={Iphone4} className="img-thumbnail" alt="" style={{ width: '80px', height: '80px', borderRadius: '12px' }} />
+          <div className="col-6 col-sm-4 mb-3">
+            <img src={Iphone4} className="img-thumbnail" alt="" style={{ width: '100%', height: 'auto', borderRadius: '12px' }} />
           </div>
-          <div className="col-4 mb-3">
-            <img src={Iphone1} className="img-thumbnail" alt="" style={{ width: '80px', height: '80px', borderRadius: '12px' }} />
+          <div className="col-6 col-sm-4 mb-3">
+            <img src={Iphone1} className="img-thumbnail" alt="" style={{ width: '100%', height: 'auto', borderRadius: '12px' }} />
           </div>
         </div>
       </div>
@@ -38,7 +39,8 @@ const bagSummary = () => {
         <span>$5849.93</span>
       </p>
       <button className="btn btn-dark d-flex align-items-center" style={{
-        width: '139px',
+        width: '100%',
+        maxWidth: '139px',
         height: '40px',
         padding: '8px 24px',
         borderRadius: '14px',
@@ -47,9 +49,7 @@ const bagSummary = () => {
         <img src={bag} alt="Cart" style={{ width: '20px', height: '20px' }} /> Checkout
       </button>
     </div>
-
-
   );
 };
 
-export default bagSummary;
+export default BagSummary;
--- melsoft-website/src/pages/bag.js
+++ melsoft-website/src/pages/bag.js
@@ -1,16 +1,20 @@
 import React from "react";
 import CheckBag from "../components/checkBag";
 import BagSummary from "../components/bagSummary";
+import SideBar from "../components/bagSideBar";
 
 
 const Bag = () => {
   return (
     <div className="d-flex" style={{
       paddingLeft: '90px'}}>
+        <div>
+          <SideBar />
+          </div>
       <div className="col-9">
         <CheckBag />
       </div>
-      <div className="col-3">
+      <div>
         <BagSummary />
       </div>
     </div>
