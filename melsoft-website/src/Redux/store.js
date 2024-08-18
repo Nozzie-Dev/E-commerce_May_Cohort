4165b304a98ba41a2c9a2277bb5a06c158702979
siphokazi <naledimuhauli@gmail.com>
2024-08-18 18:42:50 +0200

    added comments

--- melsoft-website/src/Redux/store.js
+++ melsoft-website/src/Redux/store.js
@@ -1,10 +1,23 @@
+// Import the configureStore function from @reduxjs/toolkit.
+// This function simplifies the setup of a Redux store by automatically configuring the store with good default settings.
 import { configureStore } from '@reduxjs/toolkit';
+
+// Import the cartReducer from the cartSlice file.
+// This reducer will handle all the state updates related to the cart.
 import cartReducer from './cartSlice';
 
+// Create the Redux store using configureStore.
+// The store is where all the state of the application lives.
 const store = configureStore({
+    // The reducer option allows you to define all the slices of state your application needs.
+    // Each slice is handled by a specific reducer function.
     reducer: {
+        // The 'cart' slice of state is managed by the cartReducer.
+        // In the Redux store, the cart state will be accessible under the 'cart' key.
         cart: cartReducer,
     },
 });
 
+// Export the store so it can be used throughout your application.
+// The store will be provided to the React app through the <Provider> component. which is in index.js
 export default store;
